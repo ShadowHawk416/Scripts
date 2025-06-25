c810e0adb36dc843aa720cdef9ef36ffc33f20c6
ShadowHawk416 <62296647+ShadowHawk416@users.noreply.github.com>
2025-06-25 08:30:15 -0500

    Add files via upload

---
+++ Disable-Computers.ps1
@@ -0,0 +1,12 @@
+Import-Module ActiveDirectory
+
+$DaysInactive = 90
+$time = (Get-Date).AddDays(-($DaysInactive))
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -Properties Name, OperatingSystem, SamAccountName, DistinguishedName | Select-Object Name, OperatingSystem, SamAccountName, DistinguishedName | export-csv c:\temp\inactivecomputers.csv
+
+
+$Computers = Import-Csv -LIteralPath C:\temp\inactivecomputers.csv
+
+ForEach ($Computer in $Computers) {
+    Get-ADComputer -Identity $Computer.name | Disable-ADAccount  -Confirm:$false
+}
